13ea16767d5cd1de4cfe574bf24f82445aca471d
John <ibanezman192@yahoo.gr>
2015-07-27 12:03:58 +0300

    ibnzmn

--- app/src/main/java/com/shop13/About.java
+++ app/src/main/java/com/shop13/About.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class About extends Fragment {
-//
+////
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
         return inflater.inflate(R.layout.about,container,false);
