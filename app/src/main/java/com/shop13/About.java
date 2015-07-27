bc1d3538cdc3558192351c0ba321ba5cf6be1a54
John <ibanezman192@yahoo.gr>
2015-07-27 12:03:20 +0300

    ibnzmn

--- app/src/main/java/com/shop13/About.java
+++ app/src/main/java/com/shop13/About.java
@@ -1,18 +1,13 @@
 package com.shop13;
 
 import android.app.Fragment;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class About extends Fragment {
-
+//
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
         return inflater.inflate(R.layout.about,container,false);
