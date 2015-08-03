7f0a612df9d362363a6ca91a27de2c75ea906497
nikosgig <nikosgigelos@gmail.com>
2015-08-03 10:39:36 +0300

    better cardview

--- app/src/main/java/com/shop13/About.java
+++ app/src/main/java/com/shop13/About.java
@@ -3,10 +3,16 @@ package com.shop13;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class About extends Fragment {
+
+    private Menu activityMenu;
+    private MenuItem curMenuItem;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
